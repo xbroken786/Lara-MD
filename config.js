1d6ac2996ad5cc41d451d6231d799cec7407dd5d
xbroken786 <abdullahmalik6101@gmail.com>
2024-12-31 09:06:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",   
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 ANTI_BAD: process.env.ANTI_BAD || "true",   
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 };
